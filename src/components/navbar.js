bc324a84c2a183dc91786af96a12da4eb6858468
CalvinJJ <calvinongjiajun@gmail.com>
2022-03-20 03:40:12 +0800

    1st

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <div className='logo'>
+                    <div>T.Crow</div>
+                    <div>95T</div>
+                </div>
+                
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
